71a70b91f931c6ba4216a03419bca117f9f2ad65
unknown <nqhptit@gmail.com>
2020-10-15 20:36:28 +0700

    :tada: Implement API GET /devices

---
+++ routes/devices/get.js
@@ -0,0 +1,19 @@
+import knex from '@api/database.js'
+import { BadRequestError } from '@helpers/errors'
+
+export default async (req, res) => {
+  const { deviceIds } = req.query
+
+  validateParams({ deviceIds })
+  
+  const ids = deviceIds.split(',')
+  const devices = await knex('devices').where('name', ids).select('id', 'name', 'display_name')
+
+  return res.success(devices)
+}
+
+function validateParams({ deviceIds }) {
+    if (!deviceIds) throw new BadRequestError("deviceIds not valid")
+    const deviceIdInArray = deviceIds.split(',')
+    if (!deviceIdInArray.length) throw new BadRequestError("Cannot convert deviceIds to array")
+}
