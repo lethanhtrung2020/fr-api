563991971b1dd92aff13b80a092688bb53204d1b
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-16 15:50:47 +0700

    Update get.js

--- routes/blocks/get.js
+++ routes/blocks/get.js
@@ -1,6 +1,11 @@
 import knex from '@api/database.js'
 
 export default async (res) => {
-  const blocks = await knex('blocks').select('id', 'name', 'description').whereRaw('?? = ??', ['active', 1]);
-  return res.success(blocks);
+  // const blocks = await knex('blocks').select('id', 'name', 'description').whereRaw('?? = ??', ['active', 1])
+  // return res.success(blocks)
+  return knex('blocks').select('id', 'name', 'description').whereRaw('?? = ??', ['active', 1])
+    .then(function (records) {
+      // already returns an array you can do other things here
+      return records;
+    })
 }
