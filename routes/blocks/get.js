66a0402245963592088345a5c0aa16852441d6dc
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-16 16:29:41 +0700

    Update get.js

--- routes/blocks/get.js
+++ routes/blocks/get.js
@@ -2,6 +2,6 @@ import knex from '@api/database.js'
 
 export default async (req, res) => {
   const blocks = await knex.select('id', 'name', 'description').from('blocks').where('active', '=', 1)
-    .then(data => console.log(data))
+  // .then(data => console.log(data))
   return res.success(blocks)
 }
