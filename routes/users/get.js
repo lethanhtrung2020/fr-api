67d6eb9ffeafb61c52aa64d45d55e7b84c51dfc5
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-20 13:25:42 +0700

    Update get.js

--- routes/users/get.js
+++ routes/users/get.js
@@ -11,16 +11,16 @@ export default async (req, res) => {
   const lstUsers = await knex.select("u.*", "b.name as block_name", "c.name as company_name", "f.name as floor_name", "s.name as site_name")
   .from("users as u")
   .leftJoin("blocks as b", function() {
-    this.on("b.id", "=", "u.block_id")
+    this.on("b.short_name", "=", "u.block_id")
   })
   .leftJoin("companies as c", function() {
-    this.on("c.id", "=", "u.company_id")
+    this.on("c.short_name", "=", "u.company_id")
   })
   .leftJoin("floors as f", function() {
-    this.on("f.id", "=", "u.floor_id")
+    this.on("f.short_name", "=", "u.floor_id")
   })
   .leftJoin("sites as s", function() {
-    this.on("s.id", "=", "u.site_id")
+    this.on("s.short_name", "=", "u.site_id")
   })
   .where("u.sn", device).offset(offset).limit(pageSize);
 
