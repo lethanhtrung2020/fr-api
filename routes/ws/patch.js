d1226b701e0ca7961c684671441622398f7a5dbc
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-22 16:11:34 +0700

    Update patch.js

--- routes/ws/patch.js
+++ routes/ws/patch.js
@@ -4,7 +4,12 @@ import { BadRequestError } from '@helpers/errors'
 export default async (req, res) => {
   const { type, userId, bodyTemp, roomTemp, confidence, time } = req.body
 
-  await knex.insert({ type: type, userid: userId, bodytemperature: bodyTemp, roomtemperature: roomTemp, confidence: confidence, time: time, active: 1, created_date: now(),  updated_date: now() }).into('data')
+  await knex('data')
+    .insert({ type: type, userid: userId, bodytemperature: bodyTemp, roomtemperature: roomTemp, confidence: confidence, time: time, active: 1, created_date: knex.fn.NOW(),  updated_date: knex.fn.NOW() })
+    .exec(function (err, id) {
+    console.log("Fulfilled", id);
+    process.exit(0);
+});
 
   return res.success("OK")
 }
