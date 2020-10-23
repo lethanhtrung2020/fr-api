bb8ca983b4496b683c87ffc0539f37523a49c6ca
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-23 17:03:00 +0700

    Update get.js

--- routes/devices/_deviceId/get.js
+++ routes/devices/_deviceId/get.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
   const { deviceId } = req.params
   const { name } = req.query
 
-  validateParams({ id, name })
+  validateParams({ deviceId, name })
 
   const [device] = await knex('devices').where('id', deviceId).where('name', name).select()
 
