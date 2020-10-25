8e277917fce4cfa019d29f52a0e27ca6642749b8
unknown <nqhptit@gmail.com>
2020-10-25 17:24:36 +0700

    :hammer: Fix API GET /devices to get more location names

--- routes/devices/get.js
+++ routes/devices/get.js
@@ -5,12 +5,28 @@ export default async (req, res) => {
 
   const offset = (page - 1) * pageSize
   let query = knex("devices")
+    .leftJoin("blocks", "blocks.short_name", "devices.block_id")
+    .leftJoin("sites", "sites.short_name", "devices.site_id")
+    .leftJoin("floors", "floors.short_name", "devices.floor_id")
+    .leftJoin("companies", "companies.short_name", "devices.company_id")
   if (deviceIds) {
     const ids = deviceIds.split(",")
-    query = query.whereIn("name", ids)
+    query = query.whereIn("devices.name", ids)
   }
 
-  const devices = await query.offset(offset).limit(pageSize).select("id", "name", "display_name", "block_id", "site_id", "floor_id", "company_id")
+  const devices = await query.offset(offset).limit(pageSize).select(
+    "devices.id",
+    "devices.name",
+    "devices.display_name",
+    "devices.block_id",
+    "devices.site_id",
+    "devices.floor_id",
+    "devices.company_id",
+    "blocks.name as blockName",
+    "sites.name as siteName",
+    "floors.name as floorName",
+    "companies.name as companyName"
+  )
 
   return res.success(devices);
 };
