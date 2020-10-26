0410f6493a870eb8054f002331f45a603488690a
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-26 11:44:02 +0700

    Update patch.js

--- routes/sites/_id/patch.js
+++ routes/sites/_id/patch.js
@@ -3,17 +3,17 @@ import { BadRequestError } from '@helpers/errors'
 
 export default async (req, res) => {
   const { id } = req.params
-  const { shortName } = req.query
+  const { sn } = req.query
   const { shortName, name, description } = req.body
 
-  validateParams({ id, shortName });
+  validateParams({ id, sn });
 
-  await knex('sites').where('id', id).where('short_name', shortName).update({ short_name: shortName, name: name, description: description, created_date: knex.fn.now(),  updated_date: knex.fn.now() })
+  await knex('sites').where('id', id).where('short_name', sn).update({ short_name: shortName, name: name, description: description, created_date: knex.fn.now(),  updated_date: knex.fn.now() })
 
   return res.success("OK");
 }
 
-function validateParams({ id, shortName }) {
+function validateParams({ id, sn }) {
   if (!id) throw new BadRequestError("Id not valid");
-  if (!shortName) throw new BadRequestError("Short name in query not valid");
+  if (!sn) throw new BadRequestError("Short name in query not valid");
 }
