04e40a688f9310dc6ef44a1715014414752d6ea6
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-26 11:21:15 +0700

    Update delete.js

--- routes/sites/_id/delete.js
+++ routes/sites/_id/delete.js
@@ -5,9 +5,6 @@ export default async (req, res) => {
   const { id } = req.params
 
   validateParams({ id })
-
-  const sites = await knex('sites').where('id', id).select('id')
-  const id = sites.map(site => site.id)
   await knex('sites').where('id', id).update({ active: 0, created_date: knex.fn.now(),  updated_date: knex.fn.now() })
 
   return res.success("OK");
