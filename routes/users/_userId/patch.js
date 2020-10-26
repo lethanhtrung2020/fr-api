03bfb510c3e46ad5e6bec137de2e8b551da32c11
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-26 17:14:19 +0700

    Update patch.js

--- routes/users/_userId/patch.js
+++ routes/users/_userId/patch.js
@@ -2,25 +2,25 @@ import knex from '@api/database.js'
 import { BadRequestError } from '@helpers/errors'
 
 export default async (req, res) => {
-  const { ic } = req.params
+  const { userId } = req.params
   const { devices } = req.query
-  // const { ic } = req.query
+  const { ic } = req.query
   const { blockId, companyId, siteId, floorId, cardId } = req.body
 
-  validateParams({ ic, devices })
+  validateParams({ userId, devices, ic })
   const deviceIds = devices.split(",")
 
-  const users = await knex('users').where('icCard', ic).whereIn('sn', deviceIds).select('id')
+  const users = await knex('users').where('userId', userId).where('icCard', ic).whereIn('sn', deviceIds).select('id')
   const userIds = users.map(user => user.id)
-  await knex('users').whereIn('icCard', userIds).update({ block_id: blockId, company_id: companyId, site_id: siteId, floor_id: floorId, card_id: cardId })
+  await knex('users').whereIn('userId', userIds).update({ block_id: blockId, company_id: companyId, site_id: siteId, floor_id: floorId, card_id: cardId })
 
   return res.success("OK")
 }
 
 function validateParams({ ic, devices }) {
-  // if (!id) throw new BadRequestError("User ID not valid")
-  if (!ic) throw new BadRequestError("IC Card not valid")
+  if (!userId) throw new BadRequestError("User ID not valid")
   if (!devices) throw new BadRequestError("Devices in query not valid")
+  if (!ic) throw new BadRequestError("IC Card not valid")
 
   const devicesArray = devices.split(',')
   if (!devicesArray.length) throw new BadRequestError("Cannot convert devices to array")
