b9ce8c084eca61730dd21080c0a61ef443531322
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-26 17:41:01 +0700

    Update patch.js

--- routes/users/_userId/patch.js
+++ routes/users/_userId/patch.js
@@ -4,26 +4,22 @@ import { BadRequestError } from '@helpers/errors'
 export default async (req, res) => {
   const { userId } = req.params
   const { devices } = req.query
-  const { ic } = req.query
   const { blockId, companyId, siteId, floorId, cardId } = req.body
 
-  validateParams({ userId, devices, ic })
+  validateParams({ userId, devices })
   const deviceIds = devices.split(",")
 
-  const users = await knex('users').where('userId', userId).where('icCard', ic).whereIn('sn', deviceIds).select('id')
-  Object.values(users).forEach(user => {
-    console.log(user);
-  })
+  const users = await knex('users').where('userId', userId).whereIn('sn', deviceIds).select('id')
+  
   const userIds = users.map(user => user.id)
   await knex('users').whereIn('userId', userIds).update({ block_id: blockId, company_id: companyId, site_id: siteId, floor_id: floorId, card_id: cardId })
 
   return res.success("OK")
 }
 
-function validateParams({userId, devices, ic }) {
+function validateParams({userId, devices }) {
   if (!userId) throw new BadRequestError("User ID not valid")
   if (!devices) throw new BadRequestError("Devices in query not valid")
-  if (!ic) throw new BadRequestError("IC Card not valid")
 
   const devicesArray = devices.split(',')
   if (!devicesArray.length) throw new BadRequestError("Cannot convert devices to array")
