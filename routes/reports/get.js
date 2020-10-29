272ecbb3cacaae18fd9d41515c8adf446773ca48
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-29 23:53:38 +0700

    Update get.js

--- routes/reports/get.js
+++ routes/reports/get.js
@@ -5,26 +5,27 @@ export default async (req, res) => {
   const { type='', device, site='', block='', floor='', comp='', page=1, pageSize=15 } = req.query
   validateParams({  device })
   const offset = (page-1)*pageSize
-
-  const lstReports = await knex.select("l.*", "u.name", "u.icCard", "u.phone", "u.block_id", "u.company_id", "u.floor_id", "u.site_id", "d.type as dev_type", "d.custom_name as dev_name", "b.name as block_name", "c.name as company_name", "f.name as floor_name", "s.name as site_name")
+  console.log('type: ' + `%${String(type).toUpperCase()}`)
+  const lstReports = await knex.select("l.*", "u.name", "u.icCard", "u.phone", "l.block_id", "l.company_id", "l.floor_id", "l.site_id", "d.type as dev_type", "d.custom_name as dev_name", "b.name as block_name", "c.name as company_name", "f.name as floor_name", "s.name as site_name")
   .from("detection_logs as l")
-  .leftJoin("users as u", function() {
-    this.on("l.userId", "u.userId")
-  })
+  .innerJoin("users as u")
+  // .leftJoin("users as u", function() {
+  //   this.on("l.userId", "u.userId")
+  // })
   .leftJoin("devices as d", function() {
     this.on("l.fromDevice", "d.name")
   })
   .leftJoin("blocks as b", function() {
-    this.on("b.short_name", "=", "u.block_id")
+    this.on("b.short_name", "l.block_id")
   })
   .leftJoin("companies as c", function() {
-    this.on("c.short_name", "=", "u.company_id")
+    this.on("c.short_name", "l.company_id")
   })
   .leftJoin("floors as f", function() {
-    this.on("f.short_name", "=", "u.floor_id")
+    this.on("f.short_name", "l.floor_id")
   })
   .leftJoin("sites as s", function() {
-    this.on("s.short_name", "=", "u.site_id")
+    this.on("s.short_name", "l.site_id")
   })
   .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('u.site_id', 'like', `%${site}%`).where('u.block_id', 'like', `%${block}%`).where('u.floor_id', 'like', `%${floor}%`).where('u.company_id', 'like', `%${comp}%`).offset(offset);
   // .limit(pageSize)
