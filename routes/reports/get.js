55cfdb96fdc3dd6e6b9fa8b3107b866e0ada2dc4
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-29 23:59:00 +0700

    Update get.js

--- routes/reports/get.js
+++ routes/reports/get.js
@@ -27,7 +27,7 @@ export default async (req, res) => {
   .leftJoin("sites as s", function() {
     this.on("s.short_name", "l.site_id")
   })
-  .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('u.site_id', 'like', `%${site}%`).where('u.block_id', 'like', `%${block}%`).where('u.floor_id', 'like', `%${floor}%`).where('u.company_id', 'like', `%${comp}%`).offset(offset);
+  .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('d.site_id', 'like', `%${site}%`).where('d.block_id', 'like', `%${block}%`).where('d.floor_id', 'like', `%${floor}%`).where('d.company_id', 'like', `%${comp}%`).offset(offset);
   // .limit(pageSize)
 
   return res.success(lstReports)
