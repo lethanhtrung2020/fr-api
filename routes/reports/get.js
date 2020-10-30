4c0e4c737175dfa55ef339d605e8ca870bae356f
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-10-30 21:37:09 +0700

    Update get.js

--- routes/reports/get.js
+++ routes/reports/get.js
@@ -4,7 +4,7 @@ import { BadRequestError } from '@helpers/errors'
 export default async (req, res) => {
   const {device, type='', sd='', ed='', temp = '', site='', block='', floor='', comp='', page=1, pageSize=15 } = req.query
   validateParams({  device })
-  console.log('dt: ' + new Date('2020-09-20 11:31:21').toDateString() + ' - startDate: ' + new Date(sd === '' ? new Date() : sd.toString()) + ' - endDate: ' +  new Date(ed === '' ? new Date() : ed.toString()));
+  console.log('dt: ' + new Date('2020-09-20 11:31:21').toDateString() + ' - startDate: ' + new Date(sd === '' ? new Date() : sd.toString()).toDateString() + ' - endDate: ' +  new Date(ed === '' ? new Date() : ed.toString()).toDateString());
   //console.log('compare: ' +  new Date('2020-10-30 11:31:21').toDateString() >= new Date(sd === '' ? new Date() : sd.toString()).toDateString());
   const offset = (page-1)*pageSize
 
@@ -35,7 +35,7 @@ export default async (req, res) => {
     this.on("s.short_name", "d.site_id"),
     this.on("s.active", 1)
   })
-  .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('d.site_id', 'like', `%${site}%`).where('d.block_id', 'like', `%${block}%`).where('d.floor_id', 'like', `%${floor}%`).where('d.company_id', 'like', `%${comp}%`).where(knex.raw('?', [new Date(sd===''?new Date():sd.toString()).toDateString()]), knex.raw('?', [new Date('l.detectionTime').toDateString()])).orderBy('l.detectionTime', 'desc').offset(offset);
+  .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('d.site_id', 'like', `%${site}%`).where('d.block_id', 'like', `%${block}%`).where('d.floor_id', 'like', `%${floor}%`).where('d.company_id', 'like', `%${comp}%`).where(knex.raw('?', [new Date(sd===''?new Date():sd.toString()).toDateString()]), '<=', knex.raw('?', [new Date('l.detectionTime').toDateString()])).orderBy('l.detectionTime', 'desc').offset(offset);
   // .whereBetween('l.detectionTime', [sd === '' ? new Date('1/1/1900').toString() : sd.toString(), ed === '' ? new Date().toString() : ed.toString()])
   // .where(new Date('l.detectionTime').toDateString(), '>=', knex.raw('?', new Date(sd.toString()).toDateString())).where(new Date('l.detectionTime').toDateString(), '<=', knex.raw('?', new Date(ed.toString()).toDateString()))
   //where(knex.raw('?', [new Date('l.detectionTime').toDateString()]), [knex.raw('?', [sd.toString()]), knex.raw('?', [ed.toString()])])
