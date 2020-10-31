f488cd6f1e3ed884f3cca3e166082f9165269cf7
quanghung-do <57027849+quanghung-do@users.noreply.github.com>
2020-11-01 05:07:00 +0700

    Update get.js

--- routes/reports/get.js
+++ routes/reports/get.js
@@ -5,21 +5,13 @@ import { BadRequestError } from '@helpers/errors'
 export default async (req, res) => {
   const {device, type='', sd='', ed='', temp = '', site='', block='', floor='', comp='', page=1, pageSize=15 } = req.query
   validateParams({  device })
-  // console.log('dt: ' + new Date('2020-09-20 11:31:21').toDateString() + ' - startDate: ' + new Date(sd === '' ? new Date() : sd.toString()).toDateString() + ' - endDate: ' +  new Date(ed === '' ? new Date() : ed.toString()).toDateString());
-  // console.log('compare: ' +  new Date('2020-10-20 11:31:21').toDateString() === new Date(sd === '' ? new Date() : sd.toString()).toDateString());
-  //[sd === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(sd).format('DD-MM-YYYY hh:mm'), ed === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(ed).format('DD-MM-YYYY hh:mm')]
-  //.whereRaw(moment('l.detectionTime').format('DD-MM-YYYY hh:mm') + ' BETWEEN ? AND ?', [sd === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(sd).format('DD-MM-YYYY hh:mm'), ed === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(ed).format('DD-MM-YYYY hh:mm')])
-  // console.log('sd: ' +  (sd === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(sd).format('DD-MM-YYYY hh:mm')) + ' -  ed: ' +  (ed === '' ? moment().format('DD-MM-YYYY hh:mm') : moment(ed).format('DD-MM-YYYY hh:mm')));
-  // , \'%Y-%m-%d\'
   const offset = (page-1)*pageSize
   const  strSD = !sd ? moment().subtract(7, 'days').format('YYYY-MM-DD 00:00') : sd;
   const  strED = !ed ? moment().format('YYYY-MM-DD 00:00') : sd;
-  console.log('sd: ' + strSD);
-  console.log('ed: ' +  strED);
   const lstReports = await knex.select("l.*", "u.name", "u.icCard", "u.phone", "d.block_id", "d.company_id", "d.floor_id", "d.site_id", "d.type as dev_type", "d.custom_name as dev_name", "b.name as block_name", "c.name as company_name", "f.name as floor_name", "s.name as site_name")
   .from("detection_logs as l")
   
-  .leftJoin("`users` as u", function() {
+  .leftJoin("users as u", function() {
     this.on("l.userId", "u.userId"),
     this.on("u.active", 1)
   })
@@ -44,16 +36,6 @@ export default async (req, res) => {
     this.on("s.active", 1)
   })
   .where('l.type', 'like', `%${String(type).toUpperCase()}%`).where('l.fromDevice', device).where('d.site_id', 'like', `%${site}%`).where('d.block_id', 'like', `%${block}%`).where('d.floor_id', 'like', `%${floor}%`).where('d.company_id', 'like', `%${comp}%`).whereRaw('date_format(detectionTime, \'%Y-%m-%d  %H:%i\') between cast(\''+strSD.toString()+'\' as date) and cast(\''+strED.toString()+'\' as date)').orderBy('l.detectionTime', 'desc;').offset(offset);
-  // (date_format(detectionTime, '%Y-%m-%d  %H:%i') between cast('2020-9-10 12:20' as date) and cast('2020-09-14 9:41' as date))
-  // .whereRaw('date_format(l.detectionTime, \'%Y-%m-%d %H:%i\') >= cast(\''+sd+'\' as date)').whereRaw('date_format(l.detectionTime, \'%Y-%m-%d %H:%i\') <= cast(\''+ed+'\' as date)')
-  //.whereRaw('date_format(l.detectionTime, \'%Y-%m-%d %H:%i\') between ? and ?', ['cast(\`' + sd + '\` as date)', 'cast(\`' + ed + '\` as date)'])
-  // whereRaw('date_format(l.detectionTime, \'%Y-%m-%d %H:%i\') between ? and ?', ['cast(\`' + sd + '\` as date)', 'cast(\`' + ed + '\` as date)'])
-  //date_format(detectionTime, '%Y-%m-%d') between cast('2020-9-19' as date) and cast('2020-9-19' as date)
-  // .where(new Date('l.detectionTime').toDateString(), '>=', sd)
-  // .whereBetween('l.detectionTime', [sd === '' ? new Date('1/1/1900').toString() : sd.toString(), ed === '' ? new Date().toString() : ed.toString()])
-  // .where(new Date('l.detectionTime').toDateString(), '>=', knex.raw('?', new Date(sd.toString()).toDateString())).where(new Date('l.detectionTime').toDateString(), '<=', knex.raw('?', new Date(ed.toString()).toDateString()))
-  //where(knex.raw('?', [moment('l.detectionTime').format('DD-MM-YYYY hh:mm')]), [knex.raw('?', [moment(sd).format('DD-MM-YYYY hh:mm')]), knex.raw('?', [moment(ed).format('DD-MM-YYYY hh:mm')])])
-  // .toSQL().toNative()
   return res.success(lstReports)
 }
 
