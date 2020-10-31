0423aea0ff9be4bc1bf979305fa356640d1efe39
lehuuhoainam <hoainamlehuu@gmail.com>
2020-11-01 02:18:31 +0700

    update users/_userId/patch.js

--- routes/users/_userId/patch.js
+++ routes/users/_userId/patch.js
@@ -4,14 +4,14 @@ import { BadRequestError } from '@helpers/errors'
 export default async (req, res) => {
   const { userId } = req.params
   const { devices } = req.query
-  const { blockId, companyId, siteId, floorId, cardId, icCard } = req.body
+  const { blockId, companyId, siteId, floorId, cardId, icCard, icCardExpiry } = req.body
   validateParams({ userId, devices })
   const deviceIds = devices.split(",")
 
   const users = await knex('users').where('userId', userId).whereIn('sn', deviceIds).select('id')
   
   const userIds = users.map(user => user.id)
-  await knex('users').whereIn('id', userIds).update({ block_id: blockId, company_id: companyId, site_id: siteId, floor_id: floorId, card_id: cardId, icCard })
+  await knex('users').whereIn('id', userIds).update({ block_id: blockId, company_id: companyId, site_id: siteId, floor_id: floorId, card_id: cardId, icCard, icCard_Expiry: icCardExpiry })
 
   return res.success("OK")
 }
