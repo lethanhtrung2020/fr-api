ff89eee926dc1637f33bae817c0f0ded070fd711
lethanhtrung2020 <lethanhtrung2020@gmail.com>
2020-11-01 13:00:30 +0700

    update floors

---
+++ routes/floors/_id/delete.js
@@ -0,0 +1,15 @@
+import knex from '@api/database.js'
+import { BadRequestError } from '@helpers/errors'
+
+export default async (req, res) => {
+  const { id } = req.params
+
+  validateParams({ id })
+  await knex('floors').where('id', id).update({ active: 0, created_date: knex.fn.now(),  updated_date: knex.fn.now() })
+
+  return res.success("OK");
+}
+
+function validateParams({ id, shortName }) {
+  if (!id) throw new BadRequestError("Id not valid");
+}
