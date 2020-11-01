3eaea0fb579661b3387f5b26002f170b907f5fce
lethanhtrung2020 <lethanhtrung2020@gmail.com>
2020-11-01 21:55:26 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
 import knex from "knex";
 import setupPaginator from "knex-paginator";
-import moment from 'moment';
+import moment from "moment";
 
 const connection = {
   host: "localhost",
   port: "3306",
   user: "tv",
   password: "123456!@",
-  database: "fr_db",
-  insecureAuth : true
+  database: "fr_db_test1",
+  insecureAuth : true,
   typeCast: function (field, next) {
   if (field.type == 'DATE') {
         return moment(field.string()).format('YYYY-MM-DD');
      }
      return next();
-   }
+   }  
 };
 
 const queryBuilder = knex({
